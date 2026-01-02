9657cc1aa90710d99db66223a3bc37ed91137850
ikemal12 <iltar2005@gmail.com>
2026-01-02 23:08:14 +0000

    feat: upgrade C++ version and define exception class for rom parser

--- include/cartridge.h
+++ include/cartridge.h
@@ -3,7 +3,7 @@
 #include <cstdint>
 #include <vector>
 #include <string>
-#include <optional>
+#include <stdexcept>
 
 const uint8_t NES_TAG[4] = {0x4E, 0x45, 0x53, 0x1A};
 const size_t PRG_ROM_PAGE_SIZE = 16384;
@@ -15,13 +15,19 @@ enum class Mirroring {
     FourScreen
 };
 
+class RomParseError: public std::runtime_error {
+public:
+    explicit RomParseError(const std::string& message)
+        : std::runtime_error(message) {}
+};
+
 class Rom {
 public:
     std::vector<uint8_t> prg_rom;
     std::vector<uint8_t> chr_rom;
     uint8_t mapper;
     Mirroring screen_mirroring;
-    static std::optional<Rom> create(const std::vector<uint8_t>& raw);
+    static Rom create(const std::vector<uint8_t>& raw);
     
 private:
     Rom(std::vector<uint8_t> prg, std::vector<uint8_t> chr, 
