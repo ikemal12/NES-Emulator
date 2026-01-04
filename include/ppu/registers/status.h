5c6980863e47226ba63e5d18e62839417cb918a5
ikemal12 <iltar2005@gmail.com>
2026-01-04 09:54:25 +0000

    feat(ppu): define vblank and sprite flags in status register header file

---
+++ include/ppu/registers/status.h
@@ -0,0 +1,27 @@
+#ifndef STATUS_REGISTER_H
+#define STATUS_REGISTER_H
+#include <cstdint>
+
+class StatusRegister {
+public:
+    uint8_t bits;
+    static const uint8_t NOTUSED          = 0b00000001;
+    static const uint8_t NOTUSED2         = 0b00000010;
+    static const uint8_t NOTUSED3         = 0b00000100;
+    static const uint8_t NOTUSED4         = 0b00001000;
+    static const uint8_t NOTUSED5         = 0b00010000;
+    static const uint8_t SPRITE_OVERFLOW  = 0b00100000;
+    static const uint8_t SPRITE_ZERO_HIT  = 0b01000000;
+    static const uint8_t VBLANK_STARTED   = 0b10000000;
+
+    StatusRegister();
+    bool is_in_blank() const;
+    void set_vblank_status(bool status);
+    void set_sprite_overflow(bool status);
+    void set_sprite_zero_hit(bool status);
+    void reset_vblank_status();
+    uint8_t snapshot() const;
+
+};
+
+#endif // STATUS_REGISTER_H
